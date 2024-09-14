d7a56eb2338aad1f02038fd28616d9b5d1061859
imperfection-xd <143429293+imperfection-xd@users.noreply.github.com>
2024-09-14 10:02:08 +0530

    Add files via upload

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,5 @@
+const accountId=1337
+let accountemail="xyz@gmail.com"
+var accountpassword="123456"
+accountcity="kolkata"
+console.table([accountId,accountemail,accountpassword,accountcity])
---
+++ 01_basics/03_conversion0operation.js
@@ -0,0 +1,24 @@
+let score= "true"
+console.log(typeof score)
+console.log(typeof("score")) 
+    
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//33 = 33
+//33abc = error(NaN)
+//true =1 ; false=0
+
+let isloggedIn="Rishi"
+let booleanisLoggedIn= Boolean(isloggedIn)
+console.log(booleanisLoggedIn)
+
+//1=true,0=false
+//""=flase
+// "hitesh"=true
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
